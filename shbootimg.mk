b0e801bbcd6bdf31ea701d3a9ee6960027904c55
Technohacker <technohacker1995@gmail.com>
2016-03-06 13:08:33 +0530

    galaxysl: [1/2] switch to XZ compression for ramdisk
    This is now needed to ensure that the kernel, normal ramdisk and
    TWRP's ramdisk can fit in the boot partition
    
    Change-Id: Ice37bb9026d4a09982b28fa76787280c722e0f2c

--- shbootimg.mk
+++ shbootimg.mk
@@ -2,7 +2,7 @@ uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
 $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	$(MINIGZIP) -d < $(INSTALLED_RAMDISK_TARGET) > $@
 
-MASTER_BOOTSTRAP := $(PRODUCT_OUT)/bootstrap.cpio.gz
+MASTER_BOOTSTRAP := $(PRODUCT_OUT)/bootstrap.cpio.xz
 $(MASTER_BOOTSTRAP): $(uncompressed_ramdisk) $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image
 	$(call pretty,"Bootstrap: $@")
 	cp -r device/samsung/galaxysl/bootstrap $(PRODUCT_OUT)
@@ -11,7 +11,7 @@ $(MASTER_BOOTSTRAP): $(uncompressed_ramdisk) $(recovery_uncompressed_ramdisk) $(
 	cp $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/bootstrap/stage1/
 	cp $(uncompressed_ramdisk) $(PRODUCT_OUT)/bootstrap/
 	cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/bootstrap/
-	cd $(PRODUCT_OUT)/bootstrap/; find . | cpio -o -H newc | gzip -9 > ../bootstrap.cpio.gz
+	cd $(PRODUCT_OUT)/bootstrap/; find . | cpio -o -H newc | xz -6 -Ccrc32 > ../bootstrap.cpio.xz
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(MASTER_BOOTSTRAP)

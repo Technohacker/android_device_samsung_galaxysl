ffb80281cdc4a403228a4b061af53ac1f06b7d7c
Technohacker <technohacker1995@gmail.com>
2017-05-22 11:24:33 +0530

    galaxysl: Remove reference to omap3.mk
    Change-Id: I3a27df47328daf42fec6581c82130f4a93337f29

--- galaxysl.mk
+++ galaxysl.mk
@@ -281,5 +281,5 @@ device/samsung/galaxysl/bml_over_mtd.sh:bml_over_mtd.sh
 # of the aspects that require proprietary drivers that aren't
 # commonly available
 $(call add-product-dex-preopt-module-config,services,--compiler-filter=speed)
-$(call inherit-product, hardware/ti/omap3/omap3.mk)
+#$(call inherit-product, hardware/ti/omap3/omap3.mk)
 $(call inherit-product, vendor/samsung/galaxysl/galaxysl-vendor.mk)
